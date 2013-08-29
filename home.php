342b8180f4b8fbab3ba4cf9d46f5a2e25932c563
Alex H <alexandra.higuera@gmail.com>
2013-08-29 14:48:09 -0400

    Create home.php

---
+++ home.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head align="center">
+        <title>GULSEN</title>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content="Creative CSS3 Animation Menus" />
+        <meta name="keywords" content="menu, navigation, animation, transition, transform, rotate, css3, web design, component, icon, slide" />
+        <meta name="author" content="Codrops" />
+        
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <!--<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" > 
+        <link rel="icon" href="favicon.ico" type="image/x-icon" >    -->
+        
+        <link rel="stylesheet" type="text/css" href="demo.css" />
+        <link rel="stylesheet" type="text/css" href="home.css" />
+        <link href='http://fonts.googleapis.com/css?family=Terminal+Dosis' rel='stylesheet' type='text/css' />
+    </head>
+    <body align="center">   
+        <div id = "top">
+            <center><h1>GUSLEN</h1></center>
+            <center>
+                <nav>
+                    <a href="home.php">HOME</a> |
+                    <a href="about.php">ABOUT</a> |
+                    <a href="login.php">LOGIN</a> |
+                    <a href="contact_page.php">CONTACT</a> 
+                </nav>
+            </center>
+        </div>         
+        <div class="container" style="alignment-adjust: central">
+
+            <h1>GULSEN - APP IDEA FEEDBACK</h1>
+            <div class="box">
+                <div class="content" >
+                    <ul class="ca-menu" >
+                        <li>
+                            <a href="games.php">
+                                <span class="ca-icon"><img src="games.png"/></span>
+                                <div class="ca-content">
+                                    <h2 class="ca-main"></br>Games</h2>
+    
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="utilities.php">
+                                <span class="ca-icon"><img src="utilities.png"/></span>
+                                <div class="ca-content">
+                                    <h2 class="ca-main"></br>Utilities</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="lifestyle.php">
+                                
+                                <span class="ca-icon" id="heart"><img src="lifestyle.png"/></span>
+                                <div class="ca-content">
+                                    <h2 class="ca-main"></br>Lifestyle</h2>
+                                </div>
+                            </a>
+                        </li>
+                       
+                    </ul>
+                </div><!-- content -->
+                <div class="content" >
+                    <ul class="ca-menu">
+                        <li>
+                            <a href="sports.php">
+                                <span class="ca-icon"><img src="sports.png"/></span>
+                                <div class="ca-content">
+                                    <h2 class="ca-main"></br>Sports, Health, and Fitness</h2>  
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="entertainment.php">
+                                <span class="ca-icon"><img src="entertainment.png"/></span>
+                                <div class="ca-content">
+                                    <h2 class="ca-main"></br>Entertainment and Media</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="news.php">
+                                <span class="ca-icon"><img src="news.png"/></span>
+                                <div class="ca-content">
+                                    <h2 class="ca-main"></br>News</h2>
+                                </div>
+                            </a>
+                        </li>                    
+                    </ul>
+                </div><!-- content -->
+            </div>    
+        </div>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+        </br></br></br>
+        <font color=#ff0000>
+        <?php echo "Hello World!"." son"; ?>
+        </font> 
+    </body>
+</html>
