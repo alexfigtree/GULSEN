1682404b0a0f696e2fa6491d5835f6b8115a820b
Alex H <alexandra.higuera@gmail.com>
2013-08-29 15:06:58 -0400

    Create thankyou.php

---
+++ thankyou.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>GULSEN</title>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content="Creative CSS3 Animation Menus" />
+        <meta name="keywords" content="menu, navigation, animation, transition, transform, rotate, css3, web design, component, icon, slide" />
+        <meta name="author" content="Codrops" />
+        
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <!--<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" > 
+        <link rel="icon" href="favicon.ico" type="image/x-icon" >    -->
+        
+        <link rel="stylesheet" type="text/css" href="demo.css" />
+        <link rel="stylesheet" type="text/css" href="home.css" />
+        <link href='http://fonts.googleapis.com/css?family=Terminal+Dosis' rel='stylesheet' type='text/css' />
+    </head>
+    <body>
+        <div id = "top">
+    <center><h1>GUSLEN</h1></center>
+    <center>
+        <nav>
+            <a href="home.php">HOME</a> |
+            <a href="about.php">ABOUT</a> |
+            <a href="login.php">LOGIN</a> |
+            <a href="contact_page.php">CONTACT</a> 
+        </nav>
+    </center>
+        </div>  
+        <center></br></br><h2>Thanks!  We will reply shortly.</h2></center>
+    </body>
+</html>
